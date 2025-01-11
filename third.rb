d688045eca99e6ed477c330aa9c838ae7fa3c73d
2321110001 <bu2321110001@bukkyo-u.ac.jp>
2025-01-11 05:25:13 +0000

    変更２

--- third.rb
+++ third.rb
@@ -1,9 +1,19 @@
-question = "今日の晩御飯は?"
-correct_answer = "udon"
+puts "今日の晩御飯は?"
+print "1.そば, 2.うどん, 3.パスタ : "
+answer = gets.to_i
 
+if answer == 2
+    puts "正解"
+else
+    puts "不正解"
+end
 
-puts question
+puts "今日の天気は?"
+print "1.雨, 2.晴れ, 3.雪 : "
+answer = gets.to_i
 
-user_answer = gets.chomp
-
-if ueser
+if answer == 3
+    puts "正解"
+else
+    puts "不正解"
+end
